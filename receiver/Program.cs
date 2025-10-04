9fed9e2a59992bb75bb32cc8989da2ee3724a96f
adcorduneanu <adcorduneanu@gmail.com>
2025-10-04 07:01:28 +0300

    Remove BusRunner and enhance logging configuration

--- receiver/Program.cs
+++ receiver/Program.cs
@@ -29,6 +29,8 @@
 
             var azureMonitorConnectionString = appBuilder.Configuration["ApplicationInsightsConfig:ConnectionString"];
 
+            appBuilder.Logging.ClearProviders();
+
             appBuilder.Services.AddSingleton<MassTransitErrorSink>();
 
             appBuilder.Services.AddSingleton<TenantMessageStore>();
@@ -56,9 +58,7 @@
                                .Enrich.FromLogContext()
                                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [TenantId:{TenantId}] {Message:lj}{NewLine}{Exception}")
                                .WriteTo.Sink(new MassTransitErrorSink(services), new Serilog.Configuration.BatchingOptions { BatchSizeLimit = 10, BufferingTimeLimit = TimeSpan.FromSeconds(1) }, LogEventLevel.Error);
-                       }, writeToProviders: true);
-
-            appBuilder.Services.AddHostedService<BusRunner>();
+                       }, writeToProviders: true); // forward Serilog events into Azure Monitor provider
 
             var app = appBuilder.Build();
 
